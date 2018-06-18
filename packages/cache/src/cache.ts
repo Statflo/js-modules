8dda918e3d3b8080eacc75c0eda2f07d518299d2
Helder Santana <ping@heldr.com>
2018-06-18 14:45:04 -0400

    fix typings export

--- packages/cache/src/cache.ts
+++ packages/cache/src/cache.ts
@@ -44,7 +44,7 @@ export class CacheService {
     async get<T = any>(key: string): Promise<T | void> {
         const result = await this.table.get(key);
 
-        if (result && !result.expires || result && result.expires > this.now) {
+        if (result && (!result.expires && result.expires > this.now)) {
             return result.value as T;
         }
 
